2b5a83aad42d2a25edf25f3787f72f9cd443ad12
Nicolai Frigaard <nicolai.frigaard@gmail.com>
2023-01-12 08:35:34 +0100

    refactor: shuffle around constants in file

--- src/movegen.rs
+++ src/movegen.rs
@@ -16,21 +16,175 @@ type PawnMoveBitboards = [[(Bitboard, Bitboard); BOARD_SIZE]; SIDE_COUNT];
 lazy_static! {
     static ref PIECE_MOVES: PieceMoveBitboards = init_piece_moves();
     static ref PAWN_MOVES: PawnMoveBitboards = init_pawn_moves();
-    static ref BISHOP_MOVES: MagicTable<512> = init_bishop_moves();
-    static ref ROOK_MOVES: MagicTable<4096> = init_rook_moves();
+    static ref BISHOP_MOVES: MagicTable<512> =
+        MagicTable::new(&BISHOP_DIRECTIONS, &BISHOP_MAGICS, &BISHOP_BITS);
+    static ref ROOK_MOVES: MagicTable<4096> =
+        MagicTable::new(&ROOK_DIRECTIONS, &ROOK_MAGICS, &ROOK_BITS);
 }
 
-// rook rellevant occupancy bits
+const ROOK_DIRECTIONS: [Direction; 4] = [
+    Direction::North,
+    Direction::East,
+    Direction::South,
+    Direction::West,
+];
+
+const BISHOP_DIRECTIONS: [Direction; 4] = [
+    Direction::NorthEast,
+    Direction::NorthWest,
+    Direction::SouthEast,
+    Direction::SouthWest,
+];
+
+// Relevant bits for the rook magics.
 const ROOK_BITS: [usize; BOARD_SIZE] = [
     12, 11, 11, 11, 11, 11, 11, 12, 11, 10, 10, 10, 10, 10, 10, 11, 11, 10, 10, 10, 10, 10, 10, 11,
     11, 10, 10, 10, 10, 10, 10, 11, 11, 10, 10, 10, 10, 10, 10, 11, 11, 10, 10, 10, 10, 10, 10, 11,
     11, 10, 10, 10, 10, 10, 10, 11, 12, 11, 11, 11, 11, 11, 11, 12,
 ];
 
-// bishop rellevant occupancy bits
+// Relevant bits for the bishop magics.
 const BISHOP_BITS: [usize; BOARD_SIZE] = [
     6, 5, 5, 5, 5, 5, 5, 6, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 7, 7, 7, 7, 5, 5, 5, 5, 7, 9, 9, 7, 5, 5,
     5, 5, 7, 9, 9, 7, 5, 5, 5, 5, 7, 7, 7, 7, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 6, 5, 5, 5, 5, 5, 5, 6,
+];
+
+// Pre-generated valid magic numbers for the rook.
+// Source: https://github.com/maksimKorzh/chess_programming/blob/master/src/magics/magics.c
+const ROOK_MAGICS: [u64; BOARD_SIZE] = [
+    0xa8002c000108020,
+    0x6c00049b0002001,
+    0x100200010090040,
+    0x2480041000800801,
+    0x280028004000800,
+    0x900410008040022,
+    0x280020001001080,
+    0x2880002041000080,
+    0xa000800080400034,
+    0x4808020004000,
+    0x2290802004801000,
+    0x411000d00100020,
+    0x402800800040080,
+    0xb000401004208,
+    0x2409000100040200,
+    0x1002100004082,
+    0x22878001e24000,
+    0x1090810021004010,
+    0x801030040200012,
+    0x500808008001000,
+    0xa08018014000880,
+    0x8000808004000200,
+    0x201008080010200,
+    0x801020000441091,
+    0x800080204005,
+    0x1040200040100048,
+    0x120200402082,
+    0xd14880480100080,
+    0x12040280080080,
+    0x100040080020080,
+    0x9020010080800200,
+    0x813241200148449,
+    0x491604001800080,
+    0x100401000402001,
+    0x4820010021001040,
+    0x400402202000812,
+    0x209009005000802,
+    0x810800601800400,
+    0x4301083214000150,
+    0x204026458e001401,
+    0x40204000808000,
+    0x8001008040010020,
+    0x8410820820420010,
+    0x1003001000090020,
+    0x804040008008080,
+    0x12000810020004,
+    0x1000100200040208,
+    0x430000a044020001,
+    0x280009023410300,
+    0xe0100040002240,
+    0x200100401700,
+    0x2244100408008080,
+    0x8000400801980,
+    0x2000810040200,
+    0x8010100228810400,
+    0x2000009044210200,
+    0x4080008040102101,
+    0x40002080411d01,
+    0x2005524060000901,
+    0x502001008400422,
+    0x489a000810200402,
+    0x1004400080a13,
+    0x4000011008020084,
+    0x26002114058042,
+];
+
+// Pre-generated valid magic numbers for the bishop.
+// Source: https://github.com/maksimKorzh/chess_programming/blob/master/src/magics/magics.c
+const BISHOP_MAGICS: [u64; BOARD_SIZE] = [
+    0x89a1121896040240,
+    0x2004844802002010,
+    0x2068080051921000,
+    0x62880a0220200808,
+    0x4042004000000,
+    0x100822020200011,
+    0xc00444222012000a,
+    0x28808801216001,
+    0x400492088408100,
+    0x201c401040c0084,
+    0x840800910a0010,
+    0x82080240060,
+    0x2000840504006000,
+    0x30010c4108405004,
+    0x1008005410080802,
+    0x8144042209100900,
+    0x208081020014400,
+    0x4800201208ca00,
+    0xf18140408012008,
+    0x1004002802102001,
+    0x841000820080811,
+    0x40200200a42008,
+    0x800054042000,
+    0x88010400410c9000,
+    0x520040470104290,
+    0x1004040051500081,
+    0x2002081833080021,
+    0x400c00c010142,
+    0x941408200c002000,
+    0x658810000806011,
+    0x188071040440a00,
+    0x4800404002011c00,
+    0x104442040404200,
+    0x511080202091021,
+    0x4022401120400,
+    0x80c0040400080120,
+    0x8040010040820802,
+    0x480810700020090,
+    0x102008e00040242,
+    0x809005202050100,
+    0x8002024220104080,
+    0x431008804142000,
+    0x19001802081400,
+    0x200014208040080,
+    0x3308082008200100,
+    0x41010500040c020,
+    0x4012020c04210308,
+    0x208220a202004080,
+    0x111040120082000,
+    0x6803040141280a00,
+    0x2101004202410000,
+    0x8200000041108022,
+    0x21082088000,
+    0x2410204010040,
+    0x40100400809000,
+    0x822088220820214,
+    0x40808090012004,
+    0x910224040218c9,
+    0x402814422015008,
+    0x90014004842410,
+    0x1000042304105,
+    0x10008830412a00,
+    0x2520081090008908,
+    0x40102000a0a60140,
 ];
 
 #[derive(Default)]
@@ -40,6 +194,9 @@ struct PositionBitboards {
     occupied: Bitboard,
 }
 
+// This project uses the fancy magics approach to find sliding moves. It's the
+// most common method, notably used by Stockfish.
+// https://www.chessprogramming.org/Magic_Bitboards#Fancy
 struct MagicTable<const SIZE: usize> {
     magics: [Magic; BOARD_SIZE],
     table: Vec<[Bitboard; SIZE]>,
@@ -227,187 +384,14 @@ fn init_piece_moves() -> PieceMoveBitboards {
             }
         }
 
-        let rook_directions = [
-            Direction::North,
-            Direction::East,
-            Direction::South,
-            Direction::West,
-        ];
-        moves[PieceKind::Rook as usize][i] = seek_sliding(&rook_directions, from, Bitboard(0));
-
-        let bishop_directions = [
-            Direction::NorthEast,
-            Direction::NorthWest,
-            Direction::SouthEast,
-            Direction::SouthWest,
-        ];
-        moves[PieceKind::Bishop as usize][i] = seek_sliding(&bishop_directions, from, Bitboard(0));
+        moves[PieceKind::Rook as usize][i] = seek_sliding(&ROOK_DIRECTIONS, from, Bitboard(0));
+        moves[PieceKind::Bishop as usize][i] = seek_sliding(&BISHOP_DIRECTIONS, from, Bitboard(0));
 
         moves[PieceKind::Queen as usize][i] |= moves[PieceKind::Rook as usize][i];
         moves[PieceKind::Queen as usize][i] |= moves[PieceKind::Bishop as usize][i];
     }
 
     moves
-}
-
-fn init_bishop_moves() -> MagicTable<512> {
-    println!("Searching for bishop magics");
-
-    let bishop_directions = [
-        Direction::NorthEast,
-        Direction::NorthWest,
-        Direction::SouthEast,
-        Direction::SouthWest,
-    ];
-
-    const BISHOP_MAGICS: [u64; BOARD_SIZE] = [
-        0x89a1121896040240,
-        0x2004844802002010,
-        0x2068080051921000,
-        0x62880a0220200808,
-        0x4042004000000,
-        0x100822020200011,
-        0xc00444222012000a,
-        0x28808801216001,
-        0x400492088408100,
-        0x201c401040c0084,
-        0x840800910a0010,
-        0x82080240060,
-        0x2000840504006000,
-        0x30010c4108405004,
-        0x1008005410080802,
-        0x8144042209100900,
-        0x208081020014400,
-        0x4800201208ca00,
-        0xf18140408012008,
-        0x1004002802102001,
-        0x841000820080811,
-        0x40200200a42008,
-        0x800054042000,
-        0x88010400410c9000,
-        0x520040470104290,
-        0x1004040051500081,
-        0x2002081833080021,
-        0x400c00c010142,
-        0x941408200c002000,
-        0x658810000806011,
-        0x188071040440a00,
-        0x4800404002011c00,
-        0x104442040404200,
-        0x511080202091021,
-        0x4022401120400,
-        0x80c0040400080120,
-        0x8040010040820802,
-        0x480810700020090,
-        0x102008e00040242,
-        0x809005202050100,
-        0x8002024220104080,
-        0x431008804142000,
-        0x19001802081400,
-        0x200014208040080,
-        0x3308082008200100,
-        0x41010500040c020,
-        0x4012020c04210308,
-        0x208220a202004080,
-        0x111040120082000,
-        0x6803040141280a00,
-        0x2101004202410000,
-        0x8200000041108022,
-        0x21082088000,
-        0x2410204010040,
-        0x40100400809000,
-        0x822088220820214,
-        0x40808090012004,
-        0x910224040218c9,
-        0x402814422015008,
-        0x90014004842410,
-        0x1000042304105,
-        0x10008830412a00,
-        0x2520081090008908,
-        0x40102000a0a60140,
-    ];
-
-    MagicTable::<512>::new(&bishop_directions, &BISHOP_MAGICS, &BISHOP_BITS)
-}
-
-fn init_rook_moves() -> MagicTable<4096> {
-    println!("Searching for rook magics");
-
-    let rook_directions = [
-        Direction::North,
-        Direction::East,
-        Direction::South,
-        Direction::West,
-    ];
-
-    const ROOK_MAGICS: [u64; BOARD_SIZE] = [
-        0xa8002c000108020,
-        0x6c00049b0002001,
-        0x100200010090040,
-        0x2480041000800801,
-        0x280028004000800,
-        0x900410008040022,
-        0x280020001001080,
-        0x2880002041000080,
-        0xa000800080400034,
-        0x4808020004000,
-        0x2290802004801000,
-        0x411000d00100020,
-        0x402800800040080,
-        0xb000401004208,
-        0x2409000100040200,
-        0x1002100004082,
-        0x22878001e24000,
-        0x1090810021004010,
-        0x801030040200012,
-        0x500808008001000,
-        0xa08018014000880,
-        0x8000808004000200,
-        0x201008080010200,
-        0x801020000441091,
-        0x800080204005,
-        0x1040200040100048,
-        0x120200402082,
-        0xd14880480100080,
-        0x12040280080080,
-        0x100040080020080,
-        0x9020010080800200,
-        0x813241200148449,
-        0x491604001800080,
-        0x100401000402001,
-        0x4820010021001040,
-        0x400402202000812,
-        0x209009005000802,
-        0x810800601800400,
-        0x4301083214000150,
-        0x204026458e001401,
-        0x40204000808000,
-        0x8001008040010020,
-        0x8410820820420010,
-        0x1003001000090020,
-        0x804040008008080,
-        0x12000810020004,
-        0x1000100200040208,
-        0x430000a044020001,
-        0x280009023410300,
-        0xe0100040002240,
-        0x200100401700,
-        0x2244100408008080,
-        0x8000400801980,
-        0x2000810040200,
-        0x8010100228810400,
-        0x2000009044210200,
-        0x4080008040102101,
-        0x40002080411d01,
-        0x2005524060000901,
-        0x502001008400422,
-        0x489a000810200402,
-        0x1004400080a13,
-        0x4000011008020084,
-        0x26002114058042,
-    ];
-
-    MagicTable::<4096>::new(&rook_directions, &ROOK_MAGICS, &ROOK_BITS)
 }
 
 fn init_pawn_moves() -> PawnMoveBitboards {
@@ -464,6 +448,7 @@ fn seek_sliding(directions: &[Direction], origin: Square, blockers: Bitboard) ->
     result
 }
 
+// Source: https://github.com/maksimKorzh/chess_programming/blob/master/src/magics/magics.c
 fn set_occupancy(index: u64, bitcount: usize, mut mask: Bitboard) -> Bitboard {
     let mut result = Bitboard(0);
     for i in 0..bitcount {
