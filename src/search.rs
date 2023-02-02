93cfd1225a6f403ff73f40faae3e7b9404d5db86
Nicolai Frigaard <nicolai.frigaard@gmail.com>
2023-02-02 16:06:37 +0100

    feat: add quiescence search to end of alpha beta search

--- src/search.rs
+++ src/search.rs
@@ -1,5 +1,5 @@
 use crate::board::Chessboard;
-use crate::r#move::Move;
+use crate::r#move::{Move, MoveKind};
 
 pub struct MoveSearcher {
     board: Chessboard,
@@ -40,7 +40,7 @@ impl MoveSearcher {
 
 fn alpha_beta(board: &mut Chessboard, depth: usize, mut alpha: i32, beta: i32) -> i32 {
     if depth == 0 {
-        return board.evaluate_relative();
+        return quiesce(board, -beta, -alpha);
     }
 
     let moves = board.moves().clone();
@@ -49,10 +49,36 @@ fn alpha_beta(board: &mut Chessboard, depth: usize, mut alpha: i32, beta: i32) -
         let score = -alpha_beta(board, depth - 1, -beta, -alpha);
         board.undo();
 
-        alpha = alpha.max(score);
         if score >= beta {
             return beta;
         }
+        alpha = alpha.max(score);
+    }
+
+    alpha
+}
+
+fn quiesce(board: &mut Chessboard, mut alpha: i32, beta: i32) -> i32 {
+    let moves: Vec<_> = board
+        .moves()
+        .clone()
+        .into_iter()
+        .filter(|m| matches!(m.kind, MoveKind::Capture | MoveKind::Promotion(_)))
+        .collect();
+
+    if moves.is_empty() {
+        return board.evaluate_relative();
+    }
+
+    for m in moves {
+        board.make_move(m).expect("All moves should be legal");
+        let score = -quiesce(board, -beta, -alpha);
+        board.undo();
+
+        if score >= beta {
+            return beta;
+        }
+        alpha = alpha.max(score);
     }
 
     alpha
