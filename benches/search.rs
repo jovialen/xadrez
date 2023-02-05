3da82197f5ebac8e118459c906c9a98391594c6a
Nicolai Frigaard <nicolai.frigaard@gmail.com>
2023-02-05 11:39:13 +0100

    refactor: split search benchmark into two groups

--- benches/search.rs
+++ benches/search.rs
@@ -1,3 +1,5 @@
+use std::time::Duration;
+
 use criterion::{criterion_group, criterion_main, Criterion};
 use xadrez::board::Chessboard;
 use xadrez::search::MoveSearcher;
@@ -5,13 +7,18 @@ use xadrez::search::MoveSearcher;
 fn search_move_benchmark(c: &mut Criterion) {
     let board = Chessboard::default();
 
-    let mut s = c.benchmark_group("move-search");
-    s.bench_function("search depth 1", |b| b.iter(|| search(&board, 1)));
-    s.bench_function("search depth 2", |b| b.iter(|| search(&board, 2)));
-    s.bench_function("search depth 3", |b| b.iter(|| search(&board, 3)));
-    s.bench_function("search depth 4", |b| b.iter(|| search(&board, 4)));
-    s.bench_function("search depth 5", |b| b.iter(|| search(&board, 5)));
-    s.finish();
+    let mut low_depth = c.benchmark_group("move-search-low-depth");
+    low_depth.measurement_time(Duration::from_secs(10));
+    low_depth.bench_function("search depth 1", |b| b.iter(|| search(&board, 1)));
+    low_depth.bench_function("search depth 2", |b| b.iter(|| search(&board, 2)));
+    low_depth.bench_function("search depth 3", |b| b.iter(|| search(&board, 3)));
+    low_depth.finish();
+
+    let mut high_depth = c.benchmark_group("move-search-high-depth");
+    high_depth.sample_size(10);
+    high_depth.bench_function("search depth 4", |b| b.iter(|| search(&board, 4)));
+    high_depth.bench_function("search depth 5", |b| b.iter(|| search(&board, 5)));
+    high_depth.finish();
 }
 
 #[allow(unused_must_use)]
