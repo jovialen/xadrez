60e2ef99d2ca11b1f31b9be8b18ed3fc0cf9dd68
Nicolai Frigaard <nicolai.frigaard@gmail.com>
2023-04-04 20:25:57 +0200

    feat: add evaluation for fen strings

--- src/evaluation/mod.rs
+++ src/evaluation/mod.rs
@@ -6,8 +6,23 @@ mod nnue;
 pub mod score;
 
 use crate::board::GameState;
+use crate::error::ParseFenError;
 use crate::position::Position;
 use score::{PositionPrediction, Score};
+
+/// Evaluate a fen position.
+///
+/// # Arguments
+///
+/// * `fen` - A valid fen string to evaluate.
+///
+/// # Errors
+///
+/// Will return a ParseFenError if the given FEN string is invalid.
+pub fn evaluate(fen: &str) -> Result<Score, ParseFenError> {
+    let position = Position::from_fen(fen)?;
+    Ok(evaluate_position(&position))
+}
 
 #[cfg_attr(feature = "nnue", allow(unused_variables))]
 pub(crate) fn evaluate_position(position: &Position) -> Score {
